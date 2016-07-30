577189b79c63bd502a5e666a7d762ff18a79b90a
ArsenArsen <arsen.arsenovic@hotmail.rs>
2016-07-30 22:38:17 +0200

    Place in the barebones
    Signed-off-by: ArsenArsen <arsen.arsenovic@hotmail.rs>

--- src/main/java/com/bukkitai/advancedsweardetection/Main.java
+++ src/main/java/com/bukkitai/advancedsweardetection/Main.java
@@ -1,8 +1,36 @@
 package com.bukkitai.advancedsweardetection;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+import com.bukkitai.advancedsweardetection.ai.AIThread;
+import com.bukkitai.advancedsweardetection.listeners.ChatListener;
 
+public class Main extends JavaPlugin {
+	private AIThread aiThread;
+	private static Main instance;
+	private ChatListener chatListener;
 	
+	public void onEnable() {
+		instance = this;
+		
+		aiThread = new AIThread();
+		aiThread.start();
+		
+		chatListener = new ChatListener();
+		Bukkit.getPluginManager().registerEvents(chatListener, getInstance());
+	}
+	
+	public void onDisable() {
+		instance = null;
+		aiThread.interrupt();
+	}
+	
+	public AIThread getAIThread() {
+		return aiThread;
+	}
+	
+	public static Main getInstance() {
+		return instance;
+	}
 }
