6028017a20df24eb4a75825a481bae928368f58d
RookieTEC9 <jeremiur.ps101q@gmail.com>
2016-08-02 20:12:10 +0200

    Update JaroWinkler.java

--- src/main/java/org/bukkitai/advancedsweardetection/utils/JaroWinkler.java
+++ src/main/java/org/bukkitai/advancedsweardetection/utils/JaroWinkler.java
@@ -1,16 +1,2 @@
 public class JaroWinkler {
-   public static double calc(CharSequence first, CharSequence second)
-   {
-     double DEFAULT_SCALING_FACTOR = 0.1D;
-     
-     if ((first == null) || (second == null)) {
-       throw new IllegalArgumentException("Strings must not be null");
-     }
-     
-     double jaro = score(first, second);
-     int cl = commonPrefixLength(first, second);
-     double matchScore = Math.round((jaro + 0.1D * cl * (1.0D - jaro)) * 100.0D) / 100.0D;
-     
-     return matchScore;
-   }
-}
+
