b43722af129dbbbaa838c3c3933b37ae6fe0b5ac
RookieTEC9 <jeremiur.ps101q@gmail.com>
2016-08-03 22:38:33 +0200

    edited

--- src/main/java/org/bukkitai/advancedsweardetection/commands/MainCommand.java
+++ src/main/java/org/bukkitai/advancedsweardetection/commands/MainCommand.java
@@ -9,45 +9,55 @@ import org.bukkitai.advancedsweardetection.AIConfig;
 import org.bukkitai.advancedsweardetection.Main;
 
 public class MainCommand implements CommandExecutor {
-
+	// edit
 	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		switch(args.length) {
+		switch (args.length) {
 		case 1:
-			if(args[0].equalsIgnoreCase("version") || args[0].equalsIgnoreCase("ver")) {
-				sender.sendMessage(ChatColor.DARK_AQUA + "Running version: " + ChatColor.AQUA + Main.getInstance().getDescription().getVersion());
-			} else sendHelp(sender);
+			if (args[0].equalsIgnoreCase("version") || args[0].equalsIgnoreCase("ver")) {
+				sender.sendMessage(ChatColor.DARK_AQUA + "Running version: " + ChatColor.AQUA
+						+ Main.getInstance().getDescription().getVersion());
+			} else
+				sendHelp(sender);
 			break;
 		case 2:
-			if (args[0].equalsIgnoreCase("getCount") || args[0].equalsIgnoreCase("get") || args[0].equalsIgnoreCase("data")) {
-				if (!(args.length == 1)) {
-					sender.sendMessage(ChatColor.DARK_RED + "ERROR:" + ChatColor.RED + "Syntax: /get [PLAYER] || /getCount [PLAYER]");
+			if (args[0].equalsIgnoreCase("getCount") || args[0].equalsIgnoreCase("get")
+					|| args[0].equalsIgnoreCase("data")) {
+				if (args.length < 1) {
+					sender.sendMessage(ChatColor.DARK_RED + "ERROR:" + ChatColor.RED
+							+ "Syntax: /get [PLAYER] || /getCount [PLAYER]");
 					break;
 				} else {
-				AIConfig data = new AIConfig("data.yml", Main.getInstance());
-				String path = String.valueOf(args[1]);
-				if (!sender.hasPermission("AI.checkPlayers")) break;
-				if (!Bukkit.getOfflinePlayer(args[1]).hasPlayedBefore()) {
-					sender.sendMessage(ChatColor.DARK_RED + "ERROR:" + ChatColor.RED + "That player has never played before!");
-					break;
+					AIConfig data = new AIConfig("data.yml", Main.getInstance());
+					String path = String.valueOf(args[1]);
+					if (!sender.hasPermission("AI.checkPlayers"))
+						break;
+					if (!Bukkit.getOfflinePlayer(args[1]).hasPlayedBefore()) {
+						sender.sendMessage(
+								ChatColor.DARK_RED + "ERROR:" + ChatColor.RED + "That player has never played before!");
+						break;
+					}
+					try {
+						data.getYaml().getInt(path);
+						data.reloadYaml();
+					} catch (NullPointerException e) {
+						data.getYaml().createSection(path);
+						data.reloadYaml();
+					}
+					sender.sendMessage(ChatColor.DARK_AQUA + "Player: " + args[1] + ":" + ChatColor.AQUA
+							+ String.valueOf(data.getYaml().getInt(path)));
 				}
-				try {
-					data.getYaml().getInt(path);
-				} catch (NullPointerException e) {
-					data.getYaml().createSection(path);
-				}
-				sender.sendMessage(ChatColor.DARK_AQUA + "Player: " +  args[1] + ":" + ChatColor.AQUA + String.valueOf(data.getYaml().getInt(path)));
-				}
-				} else sendHelp(sender);
+			} else
+				sendHelp(sender);
 			break;
 		case 0:
 			sendHelp(sender);
 			break;
 		default:
-			if(args[0].equalsIgnoreCase("test")) {
+			if (args[0].equalsIgnoreCase("test")) {
 				StringBuilder string = new StringBuilder();
-				for(int i = 1; i < args.length; i++) {
+				for (int i = 1; i < args.length; i++) {
 					string.append(args[i]);
 					string.append(' ');
 				}
@@ -58,7 +68,8 @@ public class MainCommand implements CommandExecutor {
 					Main.getInstance().getAIThread().addString(trim);
 					sender.sendMessage(ChatColor.RED + "Nothing found.");
 				}
-			} else sendHelp(sender);
+			} else
+				sendHelp(sender);
 			break;
 		}
 		return true;
@@ -66,10 +77,9 @@ public class MainCommand implements CommandExecutor {
 
 	private void sendHelp(CommandSender sender) {
 		sender.sendMessage(new String[] { ChatColor.DARK_AQUA + "AdvancedSwearDetection by BukkitAI Team",
-				ChatColor.DARK_AQUA + "Usage: ", 
-				ChatColor.DARK_AQUA + "/asd - " + ChatColor.AQUA + "Prints this page",
+				ChatColor.DARK_AQUA + "Usage: ", ChatColor.DARK_AQUA + "/asd - " + ChatColor.AQUA + "Prints this page",
 				ChatColor.DARK_AQUA + "/asd ver|version - " + ChatColor.AQUA + "Prints the version",
-				ChatColor.DARK_AQUA + "/asd test Wo Rd S - " + ChatColor.AQUA +"Tests a string against the database",
-				ChatColor.DARK_AQUA + "/as get|getCount|data - " + ChatColor.AQUA + "Gets a player's curse count."});
+				ChatColor.DARK_AQUA + "/asd test Wo Rd S - " + ChatColor.AQUA + "Tests a string against the database",
+				ChatColor.DARK_AQUA + "/as get|getCount|data - " + ChatColor.AQUA + "Gets a player's curse count." });
 	}
 }
