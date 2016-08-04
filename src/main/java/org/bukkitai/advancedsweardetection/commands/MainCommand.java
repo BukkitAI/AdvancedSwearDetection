6e684d739cd075ffc4bfa9c17e6de42606043b4f
RookieTEC9 <jeremiur.ps101q@gmail.com>
2016-08-04 15:04:29 +0200

    Added new color code, perms command

--- src/main/java/org/bukkitai/advancedsweardetection/commands/MainCommand.java
+++ src/main/java/org/bukkitai/advancedsweardetection/commands/MainCommand.java
@@ -1,4 +1,4 @@
-package org.bukkitai.advancedsweardetection.commands;
+packagepackage org.bukkitai.advancedsweardetection.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -9,7 +9,7 @@ import org.bukkitai.advancedsweardetection.AIConfig;
 import org.bukkitai.advancedsweardetection.Main;
 
 public class MainCommand implements CommandExecutor {
-	// edit
+	
 	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -29,32 +29,60 @@ public class MainCommand implements CommandExecutor {
 							+ "Syntax: /get [PLAYER] || /getCount [PLAYER]");
 					return true;
 				}
-					AIConfig data = new AIConfig("data.yml", Main.getInstance());
-					String path = String.valueOf(args[1]);
-					if (!sender.hasPermission("AI.checkPlayers"))
-						break;
-					if (!Bukkit.getOfflinePlayer(args[1]).hasPlayedBefore()) {
-						sender.sendMessage(
-								ChatColor.DARK_RED + "ERROR:" + ChatColor.RED + "That player has never played before!");
-						break;
-					}
-					try {
-						data.getYaml().getInt(path);
-						data.reloadYaml();
-					} catch (NullPointerException e) {
-						data.getYaml().createSection(path);
-						data.reloadYaml();
-					}
-					sender.sendMessage(ChatColor.DARK_AQUA + "Player: " + args[1] + ":" + ChatColor.AQUA
-							+ String.valueOf(data.getYaml().getInt(path)));
-				} else
+				AIConfig data = new AIConfig("data.yml", Main.getInstance());
+				String path = String.valueOf(args[1]);
+				if (!sender.hasPermission("ASD.data"))
+					break;
+				if (!Bukkit.getOfflinePlayer(args[1]).hasPlayedBefore()) {
+					sender.sendMessage(
+							ChatColor.DARK_RED + "ERROR:" + ChatColor.RED + "That player has never played before!");
+					break;
+				}
+				try {
+					data.getYaml().getInt(path);
+					data.reloadYaml();
+				} catch (NullPointerException e) {
+					data.getYaml().createSection(path);
+					data.reloadYaml();
+				}
+				sender.sendMessage(ChatColor.getByChar(Main.getInstance().getConfig().get("color1").toString().charAt(0)) + "Player: "
+						+ args[1] + ":" + ChatColor.getByChar(Main.getInstance().getConfig().get("color2").toString())
+						+ String.valueOf(data.getYaml().getInt(path)));
+			} else
 				sendHelp(sender);
 			break;
+		case 3:
+			if (args[0].equalsIgnoreCase("permissions") || args[0].equalsIgnoreCase("perm")) {
+				if (!(sender.hasPermission("ASD.perm"))) break;
+				try {
+				sender.sendMessage(new String[] {
+						org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "AdvancedSwearDetection by BukkitAI Team",
+						org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "Permissions: ",
+						org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "/asd - " + org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color2")) + "ASD.*",
+						org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "/asd ver|version - " + org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color2")) + "No permission",
+						org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "/asd test Wo Rd S - " + org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color2")) + "ASD.test",
+						org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "/asd get|getCount|data - "+ org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color2")) + "ASD.data",
+						org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "/asd permissions | perm" + org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color2")) + "ASD.perm"});
+} catch (NullPointerException e) {
+					sender.sendMessage(new String[] {
+							org.bukkit.ChatColor.DARK_AQUA + "AdvancedSwearDetection by BukkitAI Team",
+							org.bukkit.ChatColor.DARK_AQUA + "Usage: ",
+							org.bukkit.ChatColor.DARK_AQUA + "/asd - " + org.bukkit.ChatColor.AQUA + "Prints this page",
+							org.bukkit.ChatColor.DARK_AQUA + "/asd ver|version - " + org.bukkit.ChatColor.AQUA + "Prints the version",
+							org.bukkit.ChatColor.DARK_AQUA + "/asd test Wo Rd S - " + org.bukkit.ChatColor.AQUA + "Tests a string against the database",
+							org.bukkit.ChatColor.DARK_AQUA + "/asd get|getCount|data - " + org.bukkit.ChatColor.AQUA + "Gets a player's curse count." });
+							Main.getInstance().getConfig().createSection("color1");
+							Main.getInstance().getConfig().set("color1", "3");
+							Main.getInstance().getConfig().set("color2", "a");
+						}
+
+			}
 		case 0:
 			sendHelp(sender);
 			break;
 		default:
 			if (args[0].equalsIgnoreCase("test")) {
+				if (!(sender.hasPermission("ASD.test"))) break;
 				StringBuilder string = new StringBuilder();
 				for (int i = 1; i < args.length; i++) {
 					string.append(args[i]);
@@ -70,15 +98,40 @@ public class MainCommand implements CommandExecutor {
 			} else
 				sendHelp(sender);
 			break;
-		} 
+		}
 		return true;
 	}
 
 	private void sendHelp(CommandSender sender) {
-		sender.sendMessage(new String[] { org.bukkit.ChatColor.DARK_AQUA + "AdvancedSwearDetection by BukkitAI Team",
-				org.bukkit.ChatColor.DARK_AQUA + "Usage: ", org.bukkit.ChatColor.DARK_AQUA + "/asd - " + ChatColor.AQUA + "Prints this page",
-				org.bukkit.ChatColor.DARK_AQUA + "/asd ver|version - " + org.bukkit.ChatColor.AQUA + "Prints the version",
-				org.bukkit.ChatColor.DARK_AQUA + "/asd test Wo Rd S - " + org.bukkit.ChatColor.AQUA + "Tests a string against the database",
-				org.bukkit.ChatColor.DARK_AQUA + "/asd get|getCount|data - " + org.bukkit.ChatColor.AQUA + "Gets a player's curse count." });
-	}
+		try {
+		sender.sendMessage(new String[] {
+				org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "AdvancedSwearDetection by BukkitAI Team",
+				org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "Usage: ",
+				org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "/asd - " + org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color2")) + "Prints this page",
+				org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "/asd ver|version - " + org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color2")) + "Prints the version",
+				org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "/asd test Wo Rd S - " + org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color2")) + "Tests a string against the database",
+				org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color1")) + "/asd get|getCount|data - " + org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color2")) + "Gets a player's curse count.",
+				org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color")) + "/asd permissions|perm - " + org.bukkit.ChatColor.getByChar(Main.getInstance().getConfig().getString("color2")) + "Prints all permissions."
+		});
+
+		
+			} catch (NullPointerException e) {
+				sender.sendMessage(new String[] {
+						org.bukkit.ChatColor.DARK_AQUA + "AdvancedSwearDetection by BukkitAI Team",
+						org.bukkit.ChatColor.DARK_AQUA + "Usage: ",
+						org.bukkit.ChatColor.DARK_AQUA + "/asd - " + org.bukkit.ChatColor.AQUA + "Prints this page",
+						org.bukkit.ChatColor.DARK_AQUA + "/asd ver|version - " + org.bukkit.ChatColor.AQUA + "Prints the version",
+						org.bukkit.ChatColor.DARK_AQUA + "/asd test Wo Rd S - " + org.bukkit.ChatColor.AQUA + "Tests a string against the database",
+						org.bukkit.ChatColor.DARK_AQUA + "/asd get|getCount|data - " + org.bukkit.ChatColor.AQUA + "Gets a player's curse count.",
+						org.bukkit.ChatColor.DARK_AQUA + "/asd permissions|perm - " + org.bukkit.ChatColor.AQUA + "Prints all permissions."
+				});
+				Main.getInstance().getConfig().createSection("color1");
+				Main.getInstance().getConfig().createSection("color2");
+				Main.getInstance().getConfig().set("color1", "3");
+				Main.getInstance().getConfig().set("color2", "a");
+			}
+			
+
+	}	
 }
+
