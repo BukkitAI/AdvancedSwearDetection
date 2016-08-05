f92e52fe0e078c9fa88ec665b1285367c8df4527
RookieTEC9 <jeremiur.ps101q@gmail.com>
2016-08-05 10:24:56 +0200

    Update ChatListener.java

--- src/main/java/org/bukkitai/advancedsweardetection/listeners/ChatListener.java
+++ src/main/java/org/bukkitai/advancedsweardetection/listeners/ChatListener.java
@@ -4,20 +4,20 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkitai.advancedsweardetection.Main;
+import org.bukkitai.advancedsweardetection.ASD;
 import org.bukkitai.advancedsweardetection.AIConfig;
 
 public class ChatListener implements Listener {
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent event) {
-		AIConfig data = new AIConfig("data.yml", Main.getInstance());
+		AIConfig data = new AIConfig("data.yml", ASD.getInstance());
 		String path = String.valueOf(event.getPlayer().getName());
 		
-			for (String players: Main.getInstance().getConfig().getStringList("whitelist")) {
+			for (String players: ASD.getInstance().getConfig().getStringList("whitelist")) {
 				if (players.equalsIgnoreCase(event.getPlayer().getName())) return;
 			}
 
-		if (Main.getInstance().getAIThread().hasBlacklistedWord(event.getMessage())) {
+		if (ASD.getInstance().getAIThread().hasBlacklistedWord(event.getMessage())) {
 			event.setCancelled(true);
 			try {
 				int playerCount = data.getYaml().getInt(path);
